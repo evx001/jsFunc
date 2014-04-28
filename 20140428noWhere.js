1bee8a095e08703bd96aab58cc7fa356758e46eb
evx001 <eviexeris@gmail.com>
2014-04-28 15:55:36 -0400

    don't use where

---
+++ 20140428noWhere.js
@@ -0,0 +1,10 @@
+// where bad. 
+//
+function status (info) {
+
+      var w = new Widget(); 
+       w.setBackground("blue"); 
+       w.setForeground("white"); 
+       w.addText("Status: " + info); 
+       w.show(); 
+}
